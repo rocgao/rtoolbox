371c4f5a709680460ef440ca1ce6d6ca5dc70ee8
Roc On Mac <roc@mac>
2019-06-04 10:16:57 +0800

    更新time_unix命令帮助

--- command/time.go
+++ command/time.go
@@ -3,16 +3,17 @@ package command
 import (
 	"flag"
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
 )
 
 type TimeCommand struct {
 	Meta
-	help string
 
-	value string
+	help string
+	fs *flag.FlagSet
+
+	value int64
 	unit string
 	format string
 }
@@ -29,12 +30,7 @@ func (c *TimeCommand) Run(args []string) int {
 		return 1
 	}
 
-	v,err:=strconv.ParseInt(c.value,10,64)
-	if err!=nil{
-		fmt.Printf("参数value(%s)格式无效！",c.value)
-		return 1
-	}
-
+	v:=c.value
 	switch c.unit {
 	case "sec":
 		v=v*sec
@@ -43,25 +39,36 @@ func (c *TimeCommand) Run(args []string) int {
 	}
 
 	t:=time.Unix(0,v)
-	fmt.Printf("Result: %s -> %s",c.value,t.Format(c.format))
+	fmt.Printf("Result: %d -> %s",c.value,t.Format(c.format))
 
 	return 0
 }
 
 func (c *TimeCommand) Synopsis() string {
-	return "将UNIX时间戳显示为人类可读时间"
+	return "将UNIX时间戳显示为人类可读时间。执行rtoolbox time_unix --help获取该命令的详细用法"
 }
 
 func (c *TimeCommand) Help() string {
-	helpText := "将UNIX时间戳显示为人类可读时间"
-	return strings.TrimSpace(helpText)
+	c.init()
+	return c.help
 }
 
-func (c *TimeCommand) parseArgs(args []string) error{
+func (c *TimeCommand) init(){
 	fs:= flag.NewFlagSet("time_unix", flag.ContinueOnError)
-	fs.StringVar(&c.value,"value","","指定要转的时间值。")
+	fs.Int64Var(&c.value,"value",0,"指定要转的时间值。")
 	fs.StringVar(&c.unit,"unit","unix_sec","时间单位。可能值：sec,ms,ns")
 	fs.StringVar(&c.format,"format",time.RFC3339,"时间格式。")
-	return fs.Parse(args)
 
+	helpBuilder:=&strings.Builder{}
+	fs.SetOutput(helpBuilder)
+	fs.Usage()
+	c.help =helpBuilder.String()
+
+	c.fs=fs
+}
+
+
+func (c *TimeCommand) parseArgs(args []string) error{
+	c.init()
+	return c.fs.Parse(args)
 }
