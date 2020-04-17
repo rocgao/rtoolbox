f411f8b82d6f781eda5ecbb820e3a09a5c2fb4aa
高鹏 <roc.gao@msn.cn>
2020-04-17 22:53:30 +0800

    提交cmd目录

---
+++ cmd/rtoolbox/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rocgao/rtoolbox/base64en"
+	"github.com/rocgao/rtoolbox/timestamp"
+	"github.com/rocgao/rtoolbox/urlen"
+	"github.com/rocgao/rtoolbox/uuid"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	Version   = "0.0.1"
+	GitCommit = "unknown"
+)
+
+func main() {
+	cmd := rootCommand()
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+		return
+	}
+	os.Exit(0)
+}
+
+func rootCommand() *cobra.Command {
+	rootCmd := cobra.Command{
+		Use:     "rtoolbox",
+		Short:   "Roc的工具箱CLI版本",
+		Version: fmt.Sprintf("%s %s", Version, GitCommit),
+	}
+	rootCmd.AddCommand(uuid.Command(), timestamp.Command(), base64en.Command(), urlen.Command())
+	return &rootCmd
+}
