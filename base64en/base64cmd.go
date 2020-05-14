17305183fc9ac193c7e8e2ce0747c6512c491d86
roc's webIDE <roc.gao@msn.cn>
2020-05-14 11:32:33 +0800

    添加注释

--- base64en/base64cmd.go
+++ base64en/base64cmd.go
@@ -2,12 +2,12 @@ package base64en
 
 import "github.com/spf13/cobra"
 
+// Command return the command instance of base64en
 func Command() *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "base64 <command> <content>",
 		Short: "Base64编解码",
 	}
-
 	cmd.AddCommand(encoderCommand(), decoderCommand())
 	return &cmd
 }
